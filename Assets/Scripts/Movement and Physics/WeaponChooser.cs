f6f31565da0bfd62517b04ec06d88ea0e26fd3f8
Aurik-Datta <aurik.duky@gmail.com>
2020-08-21 14:32:27 -0400

    weaponchooser prelim

---
+++ Assets/Scripts/Movement and Physics/WeaponChooser.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponChooser : MonoBehaviour
+{
+    public GameObject ChooseKnife;
+
+    public GameObject ChooseFist;
+
+    public GameObject ChooseGun;
+
+    public GameObject ChooseBat;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            ChooseFist.SetActive(true);
+            ChooseKnife.SetActive(false);
+            ChooseGun.SetActive(false);
+            ChooseBat.SetActive(false);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            ChooseFist.SetActive(false);
+            ChooseKnife.SetActive(true);
+            ChooseGun.SetActive(false);
+            ChooseBat.SetActive(false);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            ChooseFist.SetActive(false);
+            ChooseKnife.SetActive(false);
+            ChooseGun.SetActive(true);
+            ChooseBat.SetActive(false);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            ChooseFist.SetActive(false);
+            ChooseKnife.SetActive(false);
+            ChooseGun.SetActive(false);
+            ChooseBat.SetActive(true);
+        }
+    }
+}
