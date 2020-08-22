bfb5890dc9290d3ed42dbdc347d2614f802061ec
Ozan Ürkmez <arkantos250@gmail.com>
2020-08-22 17:33:04 +0300

    changes before dinner

--- Assets/Scripts/Movement and Physics/CustomForce Implementations/CustomUpToMaxSpeedForce.cs
+++ Assets/Scripts/Movement and Physics/CustomForce Implementations/CustomUpToMaxSpeedForce.cs
@@ -6,13 +6,13 @@ public class CustomUpToMaxSpeedForce : ICustomForceImplementation
 {
 
     [SerializeField]
-    public Vector3 constantForce;
+    private Vector3 constantForce;
     [SerializeField]
     private float upToSpeed;
 
     public Vector3 GetCurrentForceVector(CustomForce parentForce, ForceObject objectAppliedTo)
     {
-        return (Vector3.Project(constantForce, objectAppliedTo.GetRecentNetAcceleration()).magnitude > upToSpeed)? Vector3.zero : constantForce;
+        return (Vector3.Project(constantForce, objectAppliedTo.GetRecentNetSpeed()).magnitude > upToSpeed)? Vector3.zero : constantForce;
     }
 
 
