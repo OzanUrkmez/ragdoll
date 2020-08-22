d1d35c235a893ed50e361cb7bc587580f6fb56f0
Aurik-Datta <aurik.duky@gmail.com>
2020-08-22 18:18:09 -0400

    deathcounter?

---
+++ Assets/Scripts/UI/DeathCounter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class DeathCounter : MonoBehaviour
+{
+    public Text TextChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextChange.text ="Lives: " + DeathCheck.lives.ToString();
+    }
+}
