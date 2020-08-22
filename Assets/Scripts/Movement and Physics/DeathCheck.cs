6227c517f5e2dbf2ce343a17bc7577f2efb337c7
Aurik-Datta <aurik.duky@gmail.com>
2020-08-22 17:11:06 -0400

    deathchecker online

---
+++ Assets/Scripts/Movement and Physics/DeathCheck.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class ColliderDetectorObject : MonoBehaviour
+{
+    public CharacterController CharacterController;
+    void Update()
+    {
+        
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.tag == "Death")
+        {
+            Debug.Log("Ouch");
+        }
+    }
+}
