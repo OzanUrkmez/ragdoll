252d68e57ca3c96d18f63afbb4ad5fd6b95b9134
Jilly <jillyxsong@gmail.com>
2020-08-22 19:44:57 -0400

    Main Menu

---
+++ Assets/Scripts/UI/LevelSelector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public SceneFader fader;
+    
+    public void Select (string levelName)
+    {
+        fader.FadeTo(levelName);
+    }
+}
