fe615ed43b35d7b3ba5afa944691cf8d144e4c4b
Aurik-Datta <aurik.duky@gmail.com>
2020-08-23 14:01:00 -0400

    checkpoint

--- Assets/Scripts/Movement and Physics/DeathCheck.cs
+++ Assets/Scripts/Movement and Physics/DeathCheck.cs
@@ -1,15 +1,21 @@
-﻿
-using System.Runtime.CompilerServices;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class DeathCheck : MonoBehaviour
 {
-    public static int lives = 200;
+    public static int lives = 10;
     public Transform checkpoint;
     public GameObject Player;
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
+        if (hit.collider.tag == "Checkpoint")
+        {
+            checkpoint = hit.collider.transform;
+        }
         if (hit.collider.tag == "Death")
         {
             Player.transform.position = checkpoint.transform.position;
@@ -17,6 +23,11 @@ public class DeathCheck : MonoBehaviour
             {
                 lives -= 1;
                 Debug.Log(lives);
+            }
+
+            if (lives == 0)
+            {
+                Debug.Log("Ouch");
             }
             
 
