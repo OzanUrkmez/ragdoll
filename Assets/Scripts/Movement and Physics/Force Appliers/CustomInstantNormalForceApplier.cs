6a060752951c8983e16ea73275a1b650c60875eb
Aurik-Datta <aurik.duky@gmail.com>
2020-08-23 14:02:17 -0400

    .

---
+++ Assets/Scripts/Movement and Physics/Force Appliers/CustomInstantNormalForceApplier.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomInstantNormalForceApplier : MonoBehaviour
+{
+
+    private void Start()
+    {
+    }
+
+
+    [SerializeField]
+    private float normalForceMultiplier = 1f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var forceTarget = other.GetComponent<ForceObject>();
+        if (forceTarget == null)
+            return;
+            
+    }
+
+}
