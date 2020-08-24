9b82c5d467df664189653ffcf98ac2f5962adcbd
Jilly <jillyxsong@gmail.com>
2020-08-23 23:54:10 -0400

    Ragdoll Spawn

--- Assets/Scripts/Weapons/Target.cs
+++ Assets/Scripts/Weapons/Target.cs
@@ -2,6 +2,12 @@
 
 public class Target : MonoBehaviour
 {
+    public GameObject ragdoll;
+    public Camera fpsCam;
+    public GameObject ragdoll2;
+    
+    
+
     //checks if gun has hit the object
     public void GetHit (bool hit)
     {
@@ -14,6 +20,10 @@ public class Target : MonoBehaviour
     //if gun has hit the object, excute this code
     void Die()
     {
+        ragdoll.transform.position = gameObject.transform.position;
         Destroy(gameObject);
+        ragdoll.SetActive(true);
+        ragdoll2.SetActive(true);
+        ragdoll.GetComponent<Rigidbody>().AddForce(fpsCam.transform.forward * 15000f);
     }
 }
