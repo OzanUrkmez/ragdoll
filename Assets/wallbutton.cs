83cabc06adae71b5473191878283011668974ca3
Ozan Ürkmez <arkantos250@gmail.com>
2020-08-26 11:28:14 +0300

    fix wall button

--- Assets/wallbutton.cs
+++ Assets/wallbutton.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class wallbutton : MonoBehaviour
 {
+    [SerializeField]
+    private bool deActivateOnTouch = true;
+
     public GameObject buttonwall;
+
+    private List<Transform> collidingTransforms = new List<Transform>();
+
     void OnCollisionEnter(Collision collision)
     {
-        buttonwall.SetActive(false);
+        collidingTransforms.Add(collision.transform);
+        ExecuteButtonWallLogic();
     }
 
     void OnCollisionExit(Collision collision)
     {
-        buttonwall.SetActive(true);
+        collidingTransforms.Remove(collision.transform);
+        ExecuteButtonWallLogic();
     }
+
+    private void ExecuteButtonWallLogic()
+    {
+        if(collidingTransforms.Count == 0)
+        {
+            buttonwall.SetActive(deActivateOnTouch);
+        }
+        else
+        {
+            buttonwall.SetActive(!deActivateOnTouch);
+        }
+    }
+
 }
