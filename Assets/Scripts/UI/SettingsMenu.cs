2792c540e29774d86eaa4300854f6f11feb6e715
Ozan Ürkmez <arkantos250@gmail.com>
2020-08-26 07:11:56 +0300

    double bam

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -6,11 +6,17 @@ using UnityEngine.Audio;
 public class SettingsMenu : MonoBehaviour
 {
     public AudioManager audioManager;
+
     public void SetVolume (float volumeMultiplier)
     {
         for(int i = 0; i < audioManager.sounds.Length; i++) {
             Sound sound = audioManager.sounds[i];
             sound.source.volume = sound.volume * volumeMultiplier;
         }
+    }
+
+    public void SetMusic(float musicVolumeMultiplier)
+    {
+
     }
 }
