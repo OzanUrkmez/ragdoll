a62e3a031be2b63c5084e3d9f1c8c5eb8c424e94
Aurik-Datta <aurik.duky@gmail.com>
2020-08-27 14:04:56 -0400

    maze

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    private Collider collisiCollider;
+    public GameObject text;
+    public int texton = 5;
+
+    void Start()
+    {
+        collisiCollider = GetComponent<Collider>();
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.tag == "Player")
+        {
+            text.SetActive(true);
+            Invoke("turnoff",texton);
+        }
+
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.transform.tag == "Player")
+        {
+            collisiCollider.enabled = false;
+
+        }
+    }
+
+    void turnoff()
+    {
+        text.SetActive(false);
+    }
+}
