58a288898b330d50dd47936cecdb937faadaf822
Ozan Ürkmez <arkantos250@gmail.com>
2020-08-27 22:56:56 +0300

    yes

---
+++ Assets/AudioLatePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLatePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private float secondDelay;
+
+    private void Start()
+    {
+        Invoke("PlayAudio", secondDelay);
+    }
+
+    void PlayAudio()
+    {
+        this.GetComponent<AudioSource>().Play();
+    }
+
+}
