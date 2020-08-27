6eb2b9c45fe8f13a412bdd32d3df8d4c94163c04
Jilly <jillyxsong@gmail.com>
2020-08-26 21:02:12 -0400

    Options Menu Fix

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -6,7 +6,29 @@ using UnityEngine.Audio;
 public class SettingsMenu : MonoBehaviour
 {
     public AudioManager audioManager;
-    public void SetVolume (float volumeMultiplier)
+    public AudioMixer audioMixer;
+    public GameObject pauseMenu;
+    public GameObject settingsMenu;
+    public bool menuActive = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) //Looks for the 'ESC' key input
+        {
+            if (gameObject.activeSelf)//Checks if the options menu is open
+            {
+                menuActive = true;
+            }
+
+            if (menuActive) //If ooptions menu is open, close the options menu
+            {
+                settingsMenu.SetActive(false);
+                pauseMenu.SetActive(true);
+            }
+        }
+    }
+
+    public void SetVolume (float volumeMultiplier) //Sets volume of other sound effects in audio manager
     {
         for(int i = 0; i < audioManager.sounds.Length; i++) {
             Sound sound = audioManager.sounds[i];
@@ -16,7 +38,12 @@ public class SettingsMenu : MonoBehaviour
         }
     }
 
-    public void SetMusic(float musicVolumeMultiplier)
+    public void SetVolumeOther (float volume) //Sets volume of sound sources attached to mixer
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetMusic(float musicVolumeMultiplier) //Sets volume of music
     {
         for (int i = 0; i < audioManager.sounds.Length; i++)
         {
@@ -26,4 +53,6 @@ public class SettingsMenu : MonoBehaviour
             sound.source.volume = sound.volume * musicVolumeMultiplier;
         }
     }
+
+    
 }
