285ed8e5714e37fc98f00aaed3813e4202bd17ae
Ozan Ürkmez <arkantos250@gmail.com>
2020-08-27 22:06:25 +0300

    trampoline for ma boi Aurik <3

---
+++ Assets/Scripts/Movement and Physics/Force Appliers/CustomInsantVelocitySetter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomInsantVelocitySetter : MonoBehaviour
+{
+
+    private void Start()
+    {
+        //DELETE THIS IN FUTURE? MAYBE CAN STAY
+        if((GetComponent<Rigidbody>() == null && GetComponent<Collider>() == null) || GetComponents<CustomInsantVelocitySetter>().Length != 1)
+        {
+            Destroy(this);
+        }
+    }
+
+
+    [SerializeField]
+    private Vector3 v;
+
+    #region Unity Collision Detectors
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var forceTarget = collision.collider.transform.GetComponent<ForceObject>();
+        if (forceTarget == null)
+            return;
+
+        forceTarget.DirectSetSpeed(v);
+
+    }
+
+
+    #endregion
+
+}
