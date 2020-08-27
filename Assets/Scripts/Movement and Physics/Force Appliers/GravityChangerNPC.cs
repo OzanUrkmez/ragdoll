3fda390d77f0464d53ba499e4b8ba278227eeafc
Aurik-Datta <aurik.duky@gmail.com>
2020-08-27 19:41:24 -0400

    pog

---
+++ Assets/Scripts/Movement and Physics/Force Appliers/GravityChangerNPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityChangerNPC : MonoBehaviour
+{
+    public Vector3 newGravity;
+    void Start()
+    {
+        ForceObject fo = GetComponent<ForceObject>();
+        fo.SetGravityBaseForce(newGravity);
+    }
+
+}
